37dd89c7f7c709d86fec60dadf7bac89e5500093
beenish <beeni.khurram@gmail.com>
2021-01-18 09:27:20 +0000

    added the basic input form usind bootstrap and font awsomechanges in Signup.js

--- client/src/Components/Signup.js
+++ client/src/Components/Signup.js
@@ -1,8 +1,46 @@
 import React from "react";
 
 const Signup = ()=>{
+    const signupForm =()=>(
+        <form className = "signupForm">
+            <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                    <span className="input-group-text">
+                    <i className="fa fa-user"/>
+                    </span>
+                </div>
+                <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+            </div>
+            <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                    <span className="input-group-text">
+                        <i class="fas fa-envelope"/>
+                    </span>
+                </div>
+                <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+            </div>
+            <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                    <span className="input-group-text">
+                        <i className="fas fa-key"/>
+                    </span>
+                </div>
+                <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+            </div>
+            <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                    <span className="input-group-text">
+                        <i className="fas fa-key"></i>
+                     </span>
+                </div>
+                <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+            </div>
+        </form>
+    )
+
+
     return (
-        <h3>we are in Signup component</h3>
+        <p>{signupForm()}</p>
     )
 };
 
