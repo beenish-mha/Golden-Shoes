37d0a5ad2f251942ba32aebc05e1085706f06ed2
beenish <beeni.khurram@gmail.com>
2021-01-18 19:20:45 +0000

    styling the home page changes in home.js and home.css

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,70 +1,70 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import "./home.css";
 
 const Home = ()=>{
     const showProducts = () =>(
         <div class="row">
-            <div class="col-sm-3">
-                <div class="card border-0">
-                    <div class="card-body">
-                        <Link to = "#">
+           <div class="col-sm-3">
+                <Link to href="#" class="card-links">
+                    <div class="card border-0">
+                        <div class="card-body">
                             <img className="card-img-top" src="https://images.unsplash.com/photo-1605317143724-025ceb00651c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fGdvbGRlbiUyMHNob2VzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"/>
-                        </Link>
-                        <h5 class="card-title">Golden shoe name</h5>
-                        <p class="card-text">great shoe to wear</p>
-                        <a href="#" class="btn btn-primary">Add to the cart</a>
+                            <h5 class="card-title">Golden shoe name</h5>
+                            <p class="card-text">great shoe to wear</p>
+                            <p class="card-text card-price">£65</p>
+                        </div>
                     </div>
-                </div>
+                </Link>
+                <hr/>
             </div>
             <div class="col-sm-3">
-                <div class="card border-0">
-                    <div class="card-body">
-                        <Link to = "#">
+                <Link to href="#" class="card-links">
+                    <div class="card border-0">
+                        <div class="card-body">
                             <img className="card-img-top" src="https://images.unsplash.com/photo-1605317143724-025ceb00651c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fGdvbGRlbiUyMHNob2VzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"/>
-                        </Link>
-                        <h5 class="card-title">Golden shoe name</h5>
-                        <p class="card-text">great shoe to wear</p>
-                        <a href="#" class="btn btn-primary">Add to the cart</a>
+                            <h5 class="card-title">Golden shoe name</h5>
+                            <p class="card-text">great shoe to wear</p>
+                            <p class="card-text card-price">£65</p>
+                        </div>
                     </div>
-                </div>
+                </Link>
+                <hr/>
             </div>
             <div class="col-sm-3">
-                <div class="card border-0">
-                    <div class="card-body">
-                        <Link to = "#">
+                <Link to href="#" class="card-links">
+                    <div class="card border-0">
+                        <div class="card-body">
                             <img className="card-img-top" src="https://images.unsplash.com/photo-1605317143724-025ceb00651c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fGdvbGRlbiUyMHNob2VzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"/>
-                        </Link>
-                        <h5 class="card-title">Golden shoe name</h5>
-                        <p class="card-text">great shoe to wear</p>
-                        <a href="#" class="btn btn-primary">Add to the cart</a>
+                            <h5 class="card-title">Golden shoe name</h5>
+                            <p class="card-text">great shoe to wear</p>
+                            <p class="card-text card-price">£65</p>
+                        </div>
                     </div>
-                </div>
+                </Link>
+                <hr/>
             </div>
             <div class="col-sm-3">
-                <div class="card border-0">
-                    <div class="card-body">
-                        <Link to = "#">
+                <Link to href="#" class="card-links">
+                    <div class="card border-0">
+                        <div class="card-body">
                             <img className="card-img-top" src="https://images.unsplash.com/photo-1605317143724-025ceb00651c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fGdvbGRlbiUyMHNob2VzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"/>
-                        </Link>
-                        <h5 class="card-title">Golden shoe name</h5>
-                        <p class="card-text">great shoe to wear</p>
-                        <a href="#" class="btn btn-primary">Add to the cart</a>
+                            <h5 class="card-title">Golden shoe name</h5>
+                            <p class="card-text">great shoe to wear</p>
+                            <p class="card-text card-price">£65</p>
+                        </div>
                     </div>
-                </div>
+                </Link>
+                <hr/>
             </div>
-  
         </div>
-                
-            
-
-
-            );
+   );
 
     return (
-        <>
-        <h3>we are home</h3>
+        <div>
+        <h3 className = "home-heading">Good shoes take good places</h3>
         <div>{showProducts()}</div>
-        </>
+        </div>
     )
 };
 
