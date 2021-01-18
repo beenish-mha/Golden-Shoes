1861b267c64b201845089b3dec7b15344126927c
beenish <beeni.khurram@gmail.com>
2021-01-18 18:32:13 +0000

    basic layout of home page changes in home.js

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,8 +1,70 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Home = ()=>{
+    const showProducts = () =>(
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-body">
+                        <Link to = "#">
+                            <img className="card-img-top" src="https://images.unsplash.com/photo-1605317143724-025ceb00651c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fGdvbGRlbiUyMHNob2VzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"/>
+                        </Link>
+                        <h5 class="card-title">Golden shoe name</h5>
+                        <p class="card-text">great shoe to wear</p>
+                        <a href="#" class="btn btn-primary">Add to the cart</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-body">
+                        <Link to = "#">
+                            <img className="card-img-top" src="https://images.unsplash.com/photo-1605317143724-025ceb00651c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fGdvbGRlbiUyMHNob2VzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"/>
+                        </Link>
+                        <h5 class="card-title">Golden shoe name</h5>
+                        <p class="card-text">great shoe to wear</p>
+                        <a href="#" class="btn btn-primary">Add to the cart</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-body">
+                        <Link to = "#">
+                            <img className="card-img-top" src="https://images.unsplash.com/photo-1605317143724-025ceb00651c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fGdvbGRlbiUyMHNob2VzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"/>
+                        </Link>
+                        <h5 class="card-title">Golden shoe name</h5>
+                        <p class="card-text">great shoe to wear</p>
+                        <a href="#" class="btn btn-primary">Add to the cart</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-body">
+                        <Link to = "#">
+                            <img className="card-img-top" src="https://images.unsplash.com/photo-1605317143724-025ceb00651c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fGdvbGRlbiUyMHNob2VzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"/>
+                        </Link>
+                        <h5 class="card-title">Golden shoe name</h5>
+                        <p class="card-text">great shoe to wear</p>
+                        <a href="#" class="btn btn-primary">Add to the cart</a>
+                    </div>
+                </div>
+            </div>
+  
+        </div>
+                
+            
+
+
+            );
+
     return (
+        <>
         <h3>we are home</h3>
+        <div>{showProducts()}</div>
+        </>
     )
 };
 
--- client/src/Components/Signup.js
+++ client/src/Components/Signup.js
@@ -1,8 +1,10 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import "./signup.css";
 
 const Signup = ()=>{
     const signupForm =()=>(
-        <form className = "signupForm">
+        <form className = "signupForm form-group">
             <div className="input-group mb-3">
                 <div className="input-group-prepend">
                     <span className="input-group-text">
@@ -14,18 +16,18 @@ const Signup = ()=>{
             <div className="input-group mb-3">
                 <div className="input-group-prepend">
                     <span className="input-group-text">
-                        <i class="fas fa-envelope"/>
+                        <i className="fas fa-envelope"/>
                     </span>
                 </div>
-                <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+                <input type="email" className="form-control" placeholder="Email" aria-label="Username" aria-describedby="basic-addon1"/>
             </div>
             <div className="input-group mb-3">
                 <div className="input-group-prepend">
                     <span className="input-group-text">
-                        <i className="fas fa-key"/>
+                        <i className= "fas fa-key"/>
                     </span>
                 </div>
-                <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+                <input type="password" className="form-control" placeholder="Password" aria-label="Username" aria-describedby="basic-addon1"/>
             </div>
             <div className="input-group mb-3">
                 <div className="input-group-prepend">
@@ -33,14 +35,32 @@ const Signup = ()=>{
                         <i className="fas fa-key"></i>
                      </span>
                 </div>
-                <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+                <input type="password" className="form-control" placeholder="Confirm Password" aria-label="Username" aria-describedby="basic-addon1"/>
             </div>
+            <div className="form-group">
+            <button type="submit" class="btn btn-primary btn-lg btn-block">
+               Sign Up
+            </button>
+            </div>
+            <div>
+            <p>
+                <Link to ="/signin">Sign In</Link>
+            </p>
+            </div>
+            
         </form>
     )
 
 
     return (
-        <p>{signupForm()}</p>
+        <div className= "signupContainer ">
+            <div className= " row vh-100 justify-content-md-center align-items-center px-2" >
+                <div className="col-md-5 ">
+                {signupForm()}
+                </div>
+            </div>
+         </div>
+        
     )
 };
 
