be2c35eff72954a2d4d4f87f1350afa425cb1cae
beenish <beeni.khurram@gmail.com>
2021-01-18 01:38:27 +0000

    set the margin to the left side

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -14,7 +14,7 @@ const Header = () =>{
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav navRightLinks me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li className="nav-item">
                     <Link to="#" className="nav-link" aria-current="page" >
                     Signup
