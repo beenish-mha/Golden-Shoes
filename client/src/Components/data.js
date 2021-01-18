47893cd9e70d778ac4994be9f45df2eee94e3435
beenish <beeni.khurram@gmail.com>
2021-01-18 23:55:53 +0000

    created a data file changes in data.js

---
+++ client/src/Components/data.js
@@ -0,0 +1,61 @@
+const data = {
+    shoes:[
+        {
+            Style: "heels",
+            Color: "gold",
+            Size: 5,
+            Image: "/images/pic3.jpg",
+            Quantity: 4,
+            Gender: "Female",
+            Description: "very stylish",
+            Ratings: "4stars",
+            Reviews: 8
+        },
+        {
+            Style: "Trainers",
+            Color: "gold",
+            Size: 10,
+            Image: "/images/pic2.jpg",
+            Quantity: 9,
+            Gender: "male",
+            Description: "very comfy",
+            Ratings: "4stars",
+            Reviews: 10
+        },
+        {
+            Style: "heels",
+            Color: "oat",
+            Size: 4,
+            Image: "/images/pic1.jpg",
+            Quantity: 3,
+            Gender: "Female",
+            Description: "very stylish",
+            Ratings: "5stars",
+            Reviews: 9
+        },
+        {
+            Style: "dress shoes",
+            Color: "gold",
+            Size: 3,
+            Image: "/images/pic5.jpg",
+            Quantity: 4,
+            Gender: "male",
+            Description: "very stylish",
+            Ratings: "4stars",
+            Reviews: 8
+        },
+        {
+            Style: "high heels",
+            Color: "dull gold",
+            Size: 5,
+            Image: "/images/pic4.jpg",
+            Quantity: 4,
+            Gender: "Female",
+            Description: "very stylish",
+            Ratings: "4stars",
+            Reviews: 8
+        }     
+    ]
+}
+
+export default data;
