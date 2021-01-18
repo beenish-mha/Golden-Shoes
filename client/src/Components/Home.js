d0375efd555b16120ead6eba5b8b7e08d794741e
beenish <beeni.khurram@gmail.com>
2021-01-18 18:39:51 +0000

    make the border of the card 0 changes in home.js

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -5,7 +5,7 @@ const Home = ()=>{
     const showProducts = () =>(
         <div class="row">
             <div class="col-sm-3">
-                <div class="card">
+                <div class="card border-0">
                     <div class="card-body">
                         <Link to = "#">
                             <img className="card-img-top" src="https://images.unsplash.com/photo-1605317143724-025ceb00651c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fGdvbGRlbiUyMHNob2VzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"/>
@@ -17,7 +17,7 @@ const Home = ()=>{
                 </div>
             </div>
             <div class="col-sm-3">
-                <div class="card">
+                <div class="card border-0">
                     <div class="card-body">
                         <Link to = "#">
                             <img className="card-img-top" src="https://images.unsplash.com/photo-1605317143724-025ceb00651c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fGdvbGRlbiUyMHNob2VzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"/>
@@ -29,7 +29,7 @@ const Home = ()=>{
                 </div>
             </div>
             <div class="col-sm-3">
-                <div class="card">
+                <div class="card border-0">
                     <div class="card-body">
                         <Link to = "#">
                             <img className="card-img-top" src="https://images.unsplash.com/photo-1605317143724-025ceb00651c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fGdvbGRlbiUyMHNob2VzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"/>
@@ -41,7 +41,7 @@ const Home = ()=>{
                 </div>
             </div>
             <div class="col-sm-3">
-                <div class="card">
+                <div class="card border-0">
                     <div class="card-body">
                         <Link to = "#">
                             <img className="card-img-top" src="https://images.unsplash.com/photo-1605317143724-025ceb00651c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fGdvbGRlbiUyMHNob2VzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"/>
