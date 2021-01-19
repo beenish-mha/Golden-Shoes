07399413d14748e3b96ba306a174ec1f06d5cc32
beenish <beeni.khurram@gmail.com>
2021-01-19 14:06:29 +0000

    bring the stock on the page

--- client/src/Components/ProductDetail.js
+++ client/src/Components/ProductDetail.js
@@ -1,23 +1,49 @@
 import React from "react";
 import data from "./data";
+import NotFound from "./NotFound";
+import Ratings from "./Ratings";
 
 const ProductDetail = (props)=>{
     const shoe_id = parseInt(props.match.params.id);
-    console.log (data.shoes)
     const product = data.shoes.find((shoe) =>(shoe._id) === shoe_id)
-    console.log ("nothing here"+ shoe_id + product.price)
+    if (!product){
+        return (<NotFound/>);
+    }
     return (
        
     <div className="container">
         <div className="row">
-        <div className="col-sm-6">
-        <img src={product.Image} className="img-fluid" alt={product.Style}/>
+            <div className="col-md-8">
+                <img src={product.Image} className="img-fluid" alt={product.Style}/>
+            </div>
+            <div className="col-md-4">
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <h3>{product.Color} {product.Style}</h3>
+                        <Ratings Ratings= {product.Ratings} Reviews = {product.Reviews}/>
+                    </li>
+                    <li className="list-group-item"><h3>{product.Price}</h3></li>
+                    <li className="list-group-item">
+                        <h4>Description:</h4>
+                        {product.Description}
+                    </li>
+                </ul>
+                <div className="card bg-light mb-3">
+                    <div className="card-body">
+                        <h5 className="card-title">
+                            {product.Stock<1?(
+                            <span>Out of Stock</span>): 
+                            <span>In Stock</span>}
+                        </h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            
         </div>
-        <div className="col-sm-3">Product Details</div>
-        <div className="col-sm-3">Price card</div>
     </div>
 
-        </div>
+       
 )
 }
 
