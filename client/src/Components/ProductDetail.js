4b89dbcfe979fa99600c2c85a4df93ced05e4b7b
beenish <beeni.khurram@gmail.com>
2021-01-19 15:23:48 +0000

    style the components

--- client/src/Components/ProductDetail.js
+++ client/src/Components/ProductDetail.js
@@ -19,7 +19,7 @@ const ProductDetail = (props)=>{
             <div className="col-md-4">
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item">
-                        <h3>{product.Color} {product.Style}</h3>
+                        <h3 >{product.Color} {product.Style}</h3>
                         <Ratings Ratings= {product.Ratings} Reviews = {product.Reviews}/>
                     </li>
                     <li className="list-group-item"><h3>{product.Price}</h3></li>
@@ -28,14 +28,16 @@ const ProductDetail = (props)=>{
                         {product.Description}
                     </li>
                 </ul>
-                <div className="card bg-light mb-3">
-                    <div className="card-body">
-                        <h5 className="card-title">
+                <div className="card bg-light mb-3" >
+                    <div className="card-body cart-card">
+                        <h5 className="card-title stock-check">
                             {product.Stock<1?(
                             <span>Out of Stock</span>): 
                             <span>In Stock</span>}
                         </h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <button type="button" className=" add-to-cart btn btn-primary btn-lg btn-block">Add to shopping Bag</button>
+                        
                     </div>
                 </div>
             </div>
